56b43b29204b89f386211e2a12d2a9837c6f010a
Antonio Scalia Espinoza <antoniosalvatores@gmail.com>
2022-10-22 13:10:14 -0800

    Final version for reassignments

--- reassignments.py
+++ reassignments.py
@@ -1,16 +1,16 @@
+#salvss@outlook.com
 import os as OS
 from xml.etree import ElementTree as ET
 
-filename = r'\Nacer Edit 1.xml'
-header = ""
+filename = f'\\NacerEdit1.xml'
+header = f"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!-- python script by Salvatore Scalia https://github.com/SalvatoreScalia/problem_with_waveformPro -->\n"
 
 current_path = OS.getcwd()
 print( "steop 1: " + str(current_path) + filename)
 tracktionedit_tree = ET.parse(current_path + filename)
 root = tracktionedit_tree.getroot()
 
-
-elements = tracktionedit_tree.findall('./TRACK')
+elements = root.findall('./TRACK')
 for element in elements:
     if element.get('id')=='1038':
         plugins = element.findall('./PLUGIN')
@@ -27,8 +27,8 @@ for element in elements:
                 i=i+1
 
 ET.indent(tracktionedit_tree)
-ET.register_namespace('',"Waveform 11.1.0")
-xmlstr_tracktionedit_tree = ET.tostring(tracktionedit_tree).decode()
+#ET.register_namespace('',"Waveform 11.1.0")
+xmlstr_tracktionedit_tree = ET.tostring(root).decode()
 # Create a file with header + xmlstr
 with open(OS.getcwd() + filename, "w", encoding='UTF-8') as out:
     out.write(header + xmlstr_tracktionedit_tree)
